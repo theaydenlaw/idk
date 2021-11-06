2fb0fed5f46f48572d9b0ea9f98b88585adfd501
theaydenlaw <79200037+theaydenlaw@users.noreply.github.com>
2021-11-05 22:05:51 -0230

    Add files via upload

---
+++ Stone.js
@@ -0,0 +1,11 @@
+class Stone{
+    constructor(x,y,w){
+        var options={
+            'resituation':0.8,
+            'friction':0.9,
+            'density':12
+           
+        }
+    }
+
+    }
---
+++ iron.js
@@ -0,0 +1,12 @@
+class iron{
+        constructor(x,y,r){
+            var options={
+                'resituation':0.8,
+                'friction':3,
+                'density':30
+            }
+        }
+
+
+
+}
